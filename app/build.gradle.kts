9d7bf4a4d7bdbd5869e1a02a1e680319ad03b3ff
James Traynor <Jtraynor1987@aol.com>
2026-09-07 22:27:30 +0100

    Expand live programme description on demand

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.traynor.player"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
