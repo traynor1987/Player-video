252501ea451a16670310292ff7f5255716a7c886
James Traynor <Jtraynor1987@aol.com>
2026-09-07 22:43:57 +0100

    Keep expanded programme details visible longer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.traynor.player"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
