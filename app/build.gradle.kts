9557c2e003ff9c9fb244f08a8a51e6720d763c00
James Traynor <Jtraynor1987@aol.com>
2026-09-08 00:06:22 +0100

    Release 1.0.15: fix football screen build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.traynor.player"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.0.14"
+        versionCode = 16
+        versionName = "1.0.15"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
