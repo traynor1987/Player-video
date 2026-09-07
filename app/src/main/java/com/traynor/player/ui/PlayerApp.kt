5b1703e052d3f2ce278a201a1e208bdbbde63c25
James Traynor <Jtraynor1987@aol.com>
2026-09-07 14:16:52 +0100

    Fix update screen import

--- app/src/main/java/com/traynor/player/ui/PlayerApp.kt
+++ app/src/main/java/com/traynor/player/ui/PlayerApp.kt
@@ -5,7 +5,6 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.content.pm.PackageManager
-import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedContent
