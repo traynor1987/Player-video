b52b5357be62c841cf355010451ca095440d0673
James Traynor <Jtraynor1987@aol.com>
2026-09-07 13:40:39 +0100

    Use AndroidX Media3 lint opt-in

--- app/src/main/java/com/traynor/player/ui/player/VideoPlayer.kt
+++ app/src/main/java/com/traynor/player/ui/player/VideoPlayer.kt
@@ -1,4 +1,4 @@
-@file:OptIn(androidx.media3.common.util.UnstableApi::class)
+@file:androidx.annotation.OptIn(markerClass = [androidx.media3.common.util.UnstableApi::class])
 
 package com.traynor.player.ui.player
 
